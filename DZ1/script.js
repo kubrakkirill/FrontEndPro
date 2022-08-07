0509a45306dfedaa9b5dd115b758499ca43685bc
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2022-08-07 21:01:43 +0200

    First commit

--- DZ1/script.js
+++ DZ1/script.js
@@ -4,46 +4,43 @@ let captionString = `Food prices ≠ <ВИВІД ПОТОЧНОГО ДНЯ>.<В�
     month = new Date().getMonth();
     month++;
     year = new Date().getFullYear();
-//     productDate = `Food prices for `+day+`.`+month+`.`+year
-// console.log(productDate)
 
     captionString = `Food prices ≠`+` `+day+`.`+month+`.`+year
     captionString = captionString.replace(`≠`,`-`)
 console.log(captionString)
 
-    apple = `🍎`,
-    applePrice = 10,
-    appleCount = 3,
-    appleSalePercent = 7,
+    apple = `🍎`
+    applePrice = 10
+    appleCount = 3
+    appleSalePercent = 7
 
     appleGeneralPrice = applePrice*appleCount*(1-(appleSalePercent/100))
 // console.log(appleGeneralPrice)
     appleGeneralPrice = +appleGeneralPrice.toFixed()
 
-    appleFinal = `Final price for`+` `+appleCount+` `+apple+` = `+appleGeneralPrice+` UAH`
+    appleFinal = `Final price for ${appleCount} ${apple} = ${appleGeneralPrice} UAH`
 console.log(appleFinal)
 
-    orange = `🍊`,
-    orangePrice = 12,
-    orangeCount = 2,
-    orangeSalePercent = 3,
+    orange = `🍊`
+    orangePrice = 12
+    orangeCount = 2
+    orangeSalePercent = 3
 
     orangeGeneralPrice = orangePrice*orangeCount*(1-(orangeSalePercent/100))
 // console.log(orangeFinalPrice),
     orangeGeneralPrice = +orangeGeneralPrice.toFixed()
-    orangeFinal = `Final price for`+` `+orangeCount+` `+orange+` = `+orangeGeneralPrice+` UAH`
+    orangeFinal = `Final price for ${orangeCount} ${orange} = ${orangeGeneralPrice} UAH`
 console.log(orangeFinal)
 
-    kiwi = `🥝`,
-    kiwiPrice = 15,
-    kiwiCount = 10,
+    kiwi = `🥝`
+    kiwiPrice = 15
+    kiwiCount = 10
     kiwiCountryPercent = 10;
 
     kiwiGeneralPrice = kiwiPrice*kiwiCount*(1+(kiwiCountryPercent/100))
-// console.log(kiwiFinalPrice)
     kiwiGeneralPrice = +kiwiGeneralPrice.toFixed()
-    kiwiFinal = `Final price for`+` `+kiwiCount+` `+kiwi+` = `+kiwiGeneralPrice+` UAH`
+    kiwiFinal = `Final price for ${kiwiCount} ${kiwi} = ${kiwiGeneralPrice} UAH`
 console.log(kiwiFinal)
 
-    productsGeneralPrice = `Food prices for all products = `+(appleGeneralPrice+orangeGeneralPrice+kiwiGeneralPrice)+` UAH`
+    productsGeneralPrice = `Food prices for all products = ${(appleGeneralPrice+orangeGeneralPrice+kiwiGeneralPrice)} UAH`
 console.log(productsGeneralPrice)
---
+++ DZ2/script.js
@@ -0,0 +1,51 @@
+let finalPrice = 0
+    hamburger = 10
+    cheeseburger = 15
+    doubleCheese = 5
+    potatoSmall = 10
+    potatoMiddle = 15
+    potatoBig = 20
+    ketchup = 2
+    mayonnaise  = 3
+let choiceBurger = prompt(`hamburger or cheeseburger?`)
+if (choiceBurger === null || choiceBurger ===`` || choiceBurger === `hamburger`){
+    finalPrice = finalPrice+hamburger
+} if (choiceBurger === `hamburger`)if (choiceBurger === `cheeseburger`){
+    finalPrice = finalPrice+cheeseburger
+    let choiceDoubleCheese = confirm(`Would you like to add double cheese?`)
+    if (choiceDoubleCheese){
+        finalPrice = finalPrice+doubleCheese
+    }
+}
+let choicePotato = confirm(`Would you like potato?`)
+if (choicePotato){
+    choicePotato = prompt(`Choose potato size: small/middle/big`)
+}
+if (choicePotato === null || choicePotato ===`` || choicePotato === `small`){
+    finalPrice = finalPrice+potatoSmall
+} if (choicePotato === `middle`){
+    finalPrice = finalPrice+potatoMiddle
+} if (choicePotato === `big`){
+    finalPrice = finalPrice+potatoBig
+}
+let choiceSauce = confirm(`Would you like sauce?`)
+if (choiceSauce){
+    choiceSauce = prompt(`Choose sauce: ketchup/mayonnaise`)
+}
+if (choiceSauce === null || choiceSauce ===`` || choiceSauce === `ketchup`){
+    finalPrice = finalPrice+ketchup
+} if (choiceSauce === `mayonnaise`){
+    finalPrice = finalPrice+mayonnaise
+}
+console.log(finalPrice)
+document.open();
+document.write(`<h2>Your order:</h2>
+	<ul>
+		<li>Bulka 🍔: ${choiceBurger} </li>
+		<li>Potato 🍟: ${choicePotato} </li>
+		<li>Sauce 🧂: ${choiceSauce} </li>
+	</ul>
+
+	<p>Price: ${finalPrice} </p>
+`);
+document.close();
