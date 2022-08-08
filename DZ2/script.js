83a2779bfc839059ef8c250a50fec0741c1066c5
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2022-08-08 14:35:59 +0200

    fixed bugs, add document write

--- DZ2/script.js
+++ DZ2/script.js
@@ -1,51 +1,66 @@
-let finalPrice = 0
-    hamburger = 10
-    cheeseburger = 15
-    doubleCheese = 5
-    potatoSmall = 10
-    potatoMiddle = 15
-    potatoBig = 20
-    ketchup = 2
-    mayonnaise  = 3
-let choiceBurger = prompt(`hamburger or cheeseburger?`)
-if (choiceBurger === null || choiceBurger ===`` || choiceBurger === `hamburger`){
-    finalPrice = finalPrice+hamburger
-} if (choiceBurger === `cheeseburger`){
-    finalPrice = finalPrice+cheeseburger
+let finalPrice = 0,
+    hamburger = 10,
+    cheeseburger = 15,
+    doubleCheese = 5,
+    potatoSmall = 10,
+    potatoMiddle = 15,
+    potatoBig = 20,
+    ketchup = 2,
+    mayonnaise  = 3;
+let choiceBurger = prompt(`hamburger or cheeseburger?`);
+if (choiceBurger !== null){
+    choiceBurger = choiceBurger.replaceAll(` `,``).toLowerCase();
+}
+if (choiceBurger === `cheeseburger`){
+    finalPrice += cheeseburger
     let choiceDoubleCheese = confirm(`Would you like to add double cheese?`)
     if (choiceDoubleCheese){
-        finalPrice = finalPrice+doubleCheese
+        finalPrice += doubleCheese
+    }
+} else{
+    finalPrice += hamburger;
+    choiceBurger = `hamburger`;
+}
+let choicePotato = confirm(`Would you like potato?`);
+let potato;
+
+if (choicePotato){
+    potato = prompt(`Choose potato size: small/middle/big`);
+
+    if (potato !== null){
+        potato = potato.replaceAll(` `,``).toLowerCase();
+    }
+    if (potato === `middle`){
+        finalPrice += potatoMiddle;
+    }
+    if (potato === `big`){
+        finalPrice += potatoBig;
+    } else{
+        finalPrice += potatoSmall;
+        potato = `small`;
     }
 }
-let choicePotato = confirm(`Would you like potato?`)
-if (choicePotato){
-    choicePotato = prompt(`Choose potato size: small/middle/big`)
-}
-if (choicePotato === null || choicePotato ===`` || choicePotato === `small`){
-    finalPrice = finalPrice+potatoSmall
-} if (choicePotato === `middle`){
-    finalPrice = finalPrice+potatoMiddle
-} if (choicePotato === `big`){
-    finalPrice = finalPrice+potatoBig
-}
-let choiceSauce = confirm(`Would you like sauce?`)
+let choiceSauce = confirm(`Would you like sauce?`);
+let sauce;
 if (choiceSauce){
-    choiceSauce = prompt(`Choose sauce: ketchup/mayonnaise`)
+    sauce = prompt(`Choose sauce: ketchup/mayonnaise`);
+    if (sauce !== null){
+        sauce = sauce.replaceAll(` `, ``).toLowerCase();
+    } if (sauce === `mayonnaise`){
+        finalPrice += mayonnaise;
+    } else {
+        finalPrice += ketchup;
+        sauce = `ketchup`;
+    }
 }
-if (choiceSauce === null || choiceSauce ===`` || choiceSauce === `ketchup`){
-    finalPrice = finalPrice+ketchup
-} if (choiceSauce === `mayonnaise`){
-    finalPrice = finalPrice+mayonnaise
-}
-console.log(finalPrice)
-document.open();
 document.write(`<h2>Your order:</h2>
 	<ul>
-		<li>Bulka 🍔: ${choiceBurger} </li>
-		<li>Potato 🍟: ${choicePotato} </li>
-		<li>Sauce 🧂: ${choiceSauce} </li>
-	</ul>
-
-	<p>Price: ${finalPrice} </p>
-`);
-document.close();
+		<li>Bulka 🍔: ${choiceBurger} </li>`);
+if (choicePotato){
+    document.write(`<li>Potato 🍟: ${potato} </li>`);
+}
+if (choiceSauce){
+    document.write(`<li>Sauce 🧂: ${sauce} </li>`)
+}
+document.write(`</ul>
+	<p>Price: ${finalPrice} </p>`)
