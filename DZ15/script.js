abde490bbee88a2408dc3da6545671c67db27e66
kubrakk <105593907+kubrakkirill@users.noreply.github.com>
2022-10-24 13:51:39 +0200

    first commit for DZ15

---
+++ DZ15/script.js
@@ -0,0 +1,23 @@
+let block = document.querySelector(`.block`);
+
+block.style.top = 0;
+block.style.left = 0;
+
+const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min);
+
+const getRandomColor = () => {
+    let colors = [];
+    for(let i=0; i<=2; i++){
+        colors.push(random(0, 255));
+    }
+    return `rgb(${colors.join(`,`)})`
+};
+
+const changeBlockColor = setInterval(() => block.style.backgroundColor = getRandomColor(), 500);
+
+const blockMoving = setInterval(() => {
+    let bodyWidth = document.body.offsetWidth - block.clientWidth;
+    let bodyHeight = document.body.offsetHeight - block.clientHeight;
+    block.style.top = `${random(0, bodyHeight)}px`;
+    block.style.left = `${random(0, bodyWidth)}px`;
+}, 1000)
